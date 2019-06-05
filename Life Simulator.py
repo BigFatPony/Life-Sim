349734fd504dcb0818678016bd4f482212c79ec2
Aggie M <43214029+BigFatPony@users.noreply.github.com>
2019-06-05 21:14:45 +0100

    Add files via upload

--- Life Simulator.py
+++ Life Simulator.py
@@ -3,22 +3,7 @@ print("Welcome to Aggie's Life Simulator")
 print("All these events are randomly generated")
 
 
-namelist=['John','Richard','Kevin'] #Add more names (maybe make a seperate list for male and female and randomly pick between that)
-surnamelist=[['filler']] #Work on surnames after
-#add year but make it pick a random number I guess
 
-namechoice = random.choice(namelist)
-print("Your name is",namechoice) #Later add surnames to name
-snamechoice = random.choice(surnamelist)
-
-#Large sh*thole filled with random events
-randomevents=['You read a book','You saw a cute dog','You saw a clown in the park']
-
-
-event = random.choice(randomevents)
-print(event)
-
-
-
-
+random_lines = random.choice(open("FirstNames.txt").readlines())
+print("You are called " + random_lines)
 
