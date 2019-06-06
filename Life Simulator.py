4eb475f02e83425c3485912f2d8878cfa240ae70
Aggie M <43214029+BigFatPony@users.noreply.github.com>
2019-06-06 08:37:04 +0100

    Add files via upload
    Making randomised gender with number choosing (1-2) and 2 separate lists for gender names

--- Life Simulator.py
+++ Life Simulator.py
@@ -1,12 +1,22 @@
 import random
+from random import randint
 print(" ")
 print("Welcome to Aggie's Life Simulator")
 print("All these events are randomly generated")
 print(" ")
 
-Name = random.choice(open("FirstNames.txt").readlines())
-Surname = random.choice(open("Surnames.txt").readlines())
-print("You are called " + Name.strip() + " " + Surname.strip())
+
+gender = (randint(1, 2))
+surname = random.choice(open("Surnames.txt").readlines())
+
+if gender == 1:
+    male_name = random.choice(open("MaleNames.txt").readlines())        
+    print("You are called " + male_name.strip() + " " + surname.strip())
+else:
+    female_name = random.choice(open("FemaleNames.txt").readlines())
+    print("You are called " + female_name.strip() + " " + surname.strip())
+
+
+
 Country = random.choice(open("Countries.txt").readlines())
 print("You have been born in " + Country)
-
