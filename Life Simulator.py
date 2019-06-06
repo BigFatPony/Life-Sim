2e0b227fd7e82b2e994ed4311d0a1500c0c593af
Aggie M <43214029+BigFatPony@users.noreply.github.com>
2019-06-06 14:21:16 +0100

    Add files via upload
    Extra stuff
    Enter system working

--- Life Simulator.py
+++ Life Simulator.py
@@ -5,7 +5,7 @@ print("Welcome to Aggie's Life Simulator")
 print("All these events are randomly generated")
 print(" ")
 
-
+#Beginning
 gender = (randint(1, 2))
 surname = random.choice(open("Surnames.txt").readlines())
 
@@ -16,7 +16,12 @@ else:
     female_name = random.choice(open("FemaleNames.txt").readlines())
     print("You are called " + female_name.strip() + " " + surname.strip())
 
-
-
 Country = random.choice(open("Countries.txt").readlines())
 print("You have been born in " + Country)
+
+age = 0
+input("Press Enter to age a year")
+age +=1
+print("You are now " + str(age))
+passive_event = random.choice(open("Passive_Events.txt").readlines())
+print (passive_event)
